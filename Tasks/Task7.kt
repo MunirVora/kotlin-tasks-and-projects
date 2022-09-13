96e81e5feb514a60b782a4066eeec1031bc4cbf5
MunirVora <munirvora1@gmail.com>
2022-09-13 15:27:20 +0530

    Added a new Task

---
+++ Tasks/Task7.kt
@@ -0,0 +1,24 @@
+// take 5 numbers from user and store sum of even and odd numbers separately
+import java.util.Scanner
+
+fun main(){
+    var sc = Scanner(System.`in`)
+    var i:Int
+    var evenSum:Int = 0
+    var oddSum:Int = 0
+
+    for(i in 1..5){
+        println("Enter number: ")
+        var num:Int = sc.nextInt()
+
+        if(num % 2 == 0){
+            evenSum += num
+        }
+        else {
+            oddSum += num
+        }
+    }
+
+    println("Sum of even numbers: $evenSum")
+    println("Sum of odd numbers: $oddSum")
+}
